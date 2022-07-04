7a074f71dd1a27be9352803e4fc0b74aea41bdf5
Ahmadreza Shamimi <Ahmadreza.Shamimi@gmail.com>
2022-07-04 17:41:49 -0400

    CHanges

--- lib/main.dart
+++ lib/main.dart
@@ -30,75 +30,120 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-  final int _selectedIndex = 0;
+  int _selectedIndex = 0;
+  double imageSize = 55;
 
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return MaterialApp(
-        title: 'Welcome to Flutter',
         home: Scaffold(
-          body: SafeArea(
-            child: Column(children: [
-              Container(
-                color: Colors.white,
-                width: double.infinity,
-                height: size.height / 1.8,
-                child: Stack(
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: size.height / 2.2,
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.only(
-                            bottomLeft: Radius.circular(100),
-                            bottomRight: Radius.circular(100)),
-                        image: DecorationImage(
-                          image: AssetImage(travelList[_selectedIndex].image),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
+      body: SafeArea(
+        child: Column(children: [
+          Container(
+            color: Colors.white,
+            width: double.infinity,
+            height: size.height / 1.8,
+            child: Stack(
+              children: [
+                Container(
+                  width: double.infinity,
+                  height: size.height / 2.2,
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.only(
+                        bottomLeft: Radius.circular(100),
+                        bottomRight: Radius.circular(100)),
+                    image: DecorationImage(
+                      image: AssetImage(travelList[_selectedIndex].image),
+                      fit: BoxFit.cover,
                     ),
-                    //Head Icons
-                    Positioned(
-                      right: 0,
-                      left: 0,
-                      top: 0,
-                      child: Padding(
-                        padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
-                        child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Container(
-                                width: 50,
-                                height: 50,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Color.fromARGB(85, 255, 255, 255),
-                                ),
-                                child: const Icon(CupertinoIcons.arrow_left),
-                              ),
-                              Container(
-                                width: 50,
-                                height: 50,
-                                decoration: const BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Color.fromARGB(85, 255, 255, 255),
-                                ),
-                                child: const Icon(CupertinoIcons.heart),
-                              ),
-                            ]),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-              Expanded(
-                child:
-                    Container(color: Colors.blueAccent, width: double.infinity),
-              )
-            ]),
+                //Head Icons
+                Positioned(
+                  right: 0,
+                  left: 0,
+                  top: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(16, 20, 16, 0),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color.fromARGB(85, 255, 255, 255),
+                            ),
+                            child: const Icon(CupertinoIcons.arrow_left),
+                          ),
+                          Container(
+                            width: 50,
+                            height: 50,
+                            decoration: const BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color.fromARGB(85, 255, 255, 255),
+                            ),
+                            child: const Icon(CupertinoIcons.heart),
+                          ),
+                        ]),
+                  ),
+                ),
+                Positioned(
+                  top: 80,
+                  right: 0,
+                  child: SizedBox(
+                    width: 90,
+                    height: double.maxFinite,
+                    child: ListView.builder(
+                      itemCount: travelList.length,
+                      itemBuilder: (context, index) {
+                        return imageItems(index);
+                      },
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
-        ));
+          Expanded(
+            child: Container(color: Colors.blueAccent, width: double.infinity),
+          )
+        ]),
+      ),
+    ));
+  }
+
+  Widget imageItems(int index) {
+    return Column(
+      children: [
+        Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: InkWell(
+              onTap: () {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              child: AnimatedContainer(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: _selectedIndex == index ? Colors.grey : Colors.white,
+                    width: 2,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                    image: AssetImage(travelList[index].image),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                width: _selectedIndex == index ? imageSize + 15 : imageSize,
+                height: _selectedIndex == index ? imageSize + 15 : imageSize,
+                duration: const Duration(milliseconds: 500),
+              ),
+            )),
+      ],
+    );
   }
 }
